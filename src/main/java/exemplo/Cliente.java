7a06aa90ce44915223f9edf2c362a3c0e9d6df73
Francisco Calaça Xavier <chicocx@gmail.com>
2017-03-27 19:47:59 -0300

    modificacoes

--- src/main/java/exemplo/Cliente.java
+++ src/main/java/exemplo/Cliente.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 
 public class Cliente {
 
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Servidor:");

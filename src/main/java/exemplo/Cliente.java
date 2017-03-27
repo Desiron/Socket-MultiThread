ea27a56daf843e468dfe1cf1e75aa95f8a8340f0
Francisco Calaça Xavier <chicocx@gmail.com>
2017-03-27 20:10:29 -0300

    outro commit no desenv

--- src/main/java/exemplo/Cliente.java
+++ src/main/java/exemplo/Cliente.java
@@ -10,6 +10,7 @@ public class Cliente {
     //desenvolvimento
     //outro desenvolvimento
     //outra linha
+    //nova linha
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Servidor:");

b688545cc8b5ce235534b5022eeb614fdd303662
Francisco Calaça Xavier <chicocx@gmail.com>
2017-03-27 19:50:33 -0300

    modificacoes

--- src/main/java/exemplo/Cliente.java
+++ src/main/java/exemplo/Cliente.java
@@ -1,12 +1,10 @@
 package exemplo;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
-
 
 public class Cliente {
 

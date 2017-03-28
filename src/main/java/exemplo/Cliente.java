6d2ce5f8d10e9a509aa67f3feaa78a5940df609c
Francisco Calaça Xavier <chicocx@gmail.com>
2017-03-27 21:31:46 -0300

    commit desenv

--- src/main/java/exemplo/Cliente.java
+++ src/main/java/exemplo/Cliente.java
@@ -6,7 +6,7 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 public class Cliente {
-
+    //desenvolvimento
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Servidor:");

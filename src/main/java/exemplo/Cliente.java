7500cd8a5966a5980bd79827f531d73e59528432
Francisco Calaça Xavier <chicocx@gmail.com>
2017-03-27 21:49:34 -0300

    c1

--- src/main/java/exemplo/Cliente.java
+++ src/main/java/exemplo/Cliente.java
@@ -6,7 +6,8 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 public class Cliente {
-
+    
+    //commit 1
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Servidor:");
